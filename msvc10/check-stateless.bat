122d2344b63a0a3a8857a8035401081574c6f323
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-09-08 23:55:09 +0900

    use UNCOMBINING for BIG5-HKSCS family (fixed #7)

--- msvc10/check-stateless.bat
+++ msvc10/check-stateless.bat
@@ -9,21 +9,23 @@ SET fname=%fname::=-%
 .\table-to %2 | sort > tmp-%fname%.INVERSE.TXT
 fc %1\%fname%.TXT tmp-%fname%.TXT
 
+SET uncombining=%1\%fname%.UNCOMBINING.TXT
+if not exist %uncombining% goto ELSE_2
+  SET inverse=%uncombining%
+  goto ENDIF_2
+:ELSE_2
+  SET inverse=%1\%fname%.TXT
+:ENDIF_2
+
 if not exist %1\%fname%.IRREVERSIBLE.TXT goto ELSE_1
-  copy /B %1\%fname%.TXT /B + %1\%fname%.IRREVERSIBLE.TXT /B tmp
+  copy /B %inverse% /B + %1\%fname%.IRREVERSIBLE.TXT /B tmp
   sort < tmp | uniq-u > tmp-orig-%fname%.INVERSE.TXT
   fc tmp-orig-%fname%.INVERSE.TXT tmp-%fname%.INVERSE.TXT
   del tmp
   del tmp-orig-%fname%.INVERSE.TXT
   goto ENDIF_1
 :ELSE_1
-if not exist %1\%fname%.UNCOMBINING.TXT goto ELSE_2
-  sort < %1\%fname%.UNCOMBINING.TXT | uniq-u > tmp-orig-%fname%.INVERSE.TXT
-  fc tmp-orig-%fname%.INVERSE.TXT tmp-%fname%.INVERSE.TXT
-  del tmp-orig-%fname%.INVERSE.TXT
-  goto ENDIF_1
-:ELSE_2
-  sort < %1\%fname%.TXT | uniq-u > tmp-orig-%fname%.INVERSE.TXT
+  sort < %inverse% | uniq-u > tmp-orig-%fname%.INVERSE.TXT
   fc tmp-orig-%fname%.INVERSE.TXT tmp-%fname%.INVERSE.TXT
   del tmp-orig-%fname%.INVERSE.TXT
 :ENDIF_1
