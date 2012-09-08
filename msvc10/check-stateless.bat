941710c443fd0518627c39005ecf84c382ec55d4
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-09-08 23:31:21 +0900

    move combined characters from stateless check to snippet (stateful) check (fixed #4, fixed #5)

--- msvc10/check-stateless.bat
+++ msvc10/check-stateless.bat
@@ -17,6 +17,12 @@ if not exist %1\%fname%.IRREVERSIBLE.TXT goto ELSE_1
   del tmp-orig-%fname%.INVERSE.TXT
   goto ENDIF_1
 :ELSE_1
+if not exist %1\%fname%.UNCOMBINING.TXT goto ELSE_2
+  sort < %1\%fname%.UNCOMBINING.TXT | uniq-u > tmp-orig-%fname%.INVERSE.TXT
+  fc tmp-orig-%fname%.INVERSE.TXT tmp-%fname%.INVERSE.TXT
+  del tmp-orig-%fname%.INVERSE.TXT
+  goto ENDIF_1
+:ELSE_2
   sort < %1\%fname%.TXT | uniq-u > tmp-orig-%fname%.INVERSE.TXT
   fc tmp-orig-%fname%.INVERSE.TXT tmp-%fname%.INVERSE.TXT
   del tmp-orig-%fname%.INVERSE.TXT
